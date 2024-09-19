07d47057b7583a93b08d9e4641c48259a2d08183
JP Lomas <jp.lomas@gmail.com>
2024-09-19 12:41:23 +0100

    Add check for schema call in API
    Signed-off-by: JP Lomas <jp.lomas@gmail.com>

--- test/api.test.mjs
+++ test/api.test.mjs
@@ -30,3 +30,29 @@ describe('Request to / returns the interactive front end', () => {
     expect(body).to.have.string('<h1>NELA API</h1>');
   });
 });
+
+describe('Request to /schema.json returns the expected schema in JSON', () => {
+  let response;
+  let body;
+
+  beforeAll(async () => {
+    response = await fetch('http://localhost:3000/schema.json');
+    body = await response.json();
+  }, BEFORE_ALL_TIMEOUT);
+
+  test('Should have response status 200', () => {
+    expect(response.status).toBe(200);
+  });
+
+  test('Should have content-type JSON', () => {
+    expect(response.headers.get('Content-Type')).toBe('application/json; charset=UTF-8');
+  });
+
+  test('Should have array in the body', () => {
+    expectTypeOf(body).toBeArray();
+  });
+
+  test('$schema key should be http://json-schema.org/draft-07/schema#', () => {
+    expect(body['$schema']).to.have.string('http://json-schema.org/draft-07/schema#');
+  });
+});
