f5ff4853f84e23ca13a0f99795cc8caa0c5f0190
JP Lomas <jp.lomas@gmail.com>
2025-09-28 11:43:32 +0100

    Lint fix
    Signed-off-by: JP Lomas <jp.lomas@gmail.com>

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -2,13 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   test: {
-    exclude: [
-      'coverage',
-      '**/node_modules/**',
-      'dist',
-      'src/index.js',
-      'frontend/node_modules/**'
-    ],
+    exclude: ['coverage', '**/node_modules/**', 'dist', 'src/index.js', 'frontend/node_modules/**'],
     coverage: {
       provider: 'istanbul',
       include: ['src/**/*'],
